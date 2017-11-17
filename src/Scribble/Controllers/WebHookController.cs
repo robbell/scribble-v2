0d7fc2c36b77447e4608a710259b88663cfa19eb
Rob Bell <rob.bell@asos.com>
2017-11-17 17:13:05 +0000

    Add first pass web hook event classes

---
+++ src/Scribble/Controllers/WebHookController.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Scribble.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/WebHook")]
+    public class WebHookController : Controller
+    {
+        [HttpPost]
+        public void Post([FromBody]WebHookEvent webHookEvent)
+        {
+        }
+    }
+
+    public class WebHookEvent
+    {
+        [JsonProperty("action")]
+        public string Action { get; set; }
+
+        [JsonProperty("repository")]
+        public Repository Repository { get; set; }
+    }
+
+    public class Repository
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("full_name")]
+        public string FullName { get; set; }
+    }
+}
