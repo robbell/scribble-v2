8114dee80951b8488cf5b438abbafcb6531880b2
Rob Bell <rob.bell@asos.com>
2018-04-27 14:57:23 +0100

    Add implementation for Markdown conversion

---
+++ src/Scribble.Tests/ContractTests/MarkdownConverterShould.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Scribble.Web.Domain;
+using Xunit;
+
+namespace Scribble.Tests.ContractTests
+{
+    public class MarkdownConverterShould
+    {
+        [Fact, Trait("Category", "Integration")]
+        public void ConvertMarkdownToHtml()
+        {
+            var markdownConverter = new MarkdownConverter();
+
+            var result = markdownConverter.Convert("# Test");
+
+            result.Should().Be("<h1>Test</h1>\r\n");
+        }
+    }
+}
---
+++ src/Scribble.Web/Domain/MarkdownConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Scribble.Web.Domain
+{
+    public class MarkdownConverter : IMarkdownConverter
+    {
+        public string Convert(string input)
+        {
+            return CommonMark.CommonMarkConverter.Convert(input);
+        }
+    }
+}
